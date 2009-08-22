e82c728910c76d192ee31f891919cc9ba884ec56
Alexandre Girard <alx.girard@gmail.com>
2009-08-22 15:55:25 +0200

    first commit

---
+++ wp-content/plugins/photoq-photoblog-plugin/panels/options.php
@@ -0,0 +1,256 @@
+<div class="wrap">
+	<form method="post" action="options-general.php?page=whoismanu-photoq.php">
+		
+		<h2>PhotoQ Options</h2>
+		
+		<p class="submit top-savebtn">
+			<input type="submit" name="info_update" class="button-primary"
+				value="<?php _e('Save Changes', 'PhotoQ') ?>" />
+		</p>
+			
+		
+				
+			<div id="poststuff">
+			
+			
+			<div  class="postbox ">
+			<h3 class="postbox-handle"><span><?php _e('Image sizes', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			
+			
+			<?php $this->_oc->render('imageSizes');?>
+			
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table noborder">
+			<tr valign="top">
+					<th scope="row">
+						<label for="newImageSizeName">Name of new image size: </label>
+					</th>
+					<td>
+			<input type="text" name="newImageSizeName" id="newImageSizeName"
+					size="20" maxlength="20" value="" />
+			<input type="submit" class="button-secondary"
+					name="addImageSize"
+					value="<?php _e('Add Image Size', 'PhotoQ') ?> &raquo;" />
+			
+					</td>
+			</tr>
+			</table>
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table noborder">
+			<tr valign="top">
+				<th scope="row"><?php _e('Hide \'original\' folder:', 'PhotoQ'); ?></th>
+				<td>
+				<?php 
+					$this->_oc->render('originalFolder');
+					
+					$folderName = get_option('wimpq_originalFolder');
+					$folderName = $folderName ? $folderName : 'original';
+					echo '<br/>(Current name: '.$folderName.')';
+				?></td>
+			</tr>
+			</table>
+			</div>
+			</div>
+			
+			<div  class="postbox ">
+			<h3 class="postbox-handle"><span><?php _e('Views', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table noborder">
+				<tr valign="top">
+					<th>the_content</th>
+					<td></td>
+				</tr>
+				<?php $this->_oc->render('contentView');?>
+			</table>	
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table noborder">
+				<tr valign="top">
+					<th>the_excerpt</th>
+					<td></td>
+				</tr>
+				<?php $this->_oc->render('excerptView');?>
+			</table>
+			</div>
+			</div>
+			
+			
+			<div  class="postbox closed">
+			<h3 class="postbox-handle"><span><?php _e('Exif', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+				<tr valign="top">
+					<th scope="row"><?php _e('Choose Exif Tags:', 'PhotoQ'); ?></th>
+					<td>
+						<?php 
+							if(!get_option( "wimpq_exif_tags" )) 
+								_e('Not tags yet. PhotoQ will learn exif tags from uploaded photos. Upload a photo first,
+								then come back and choose your exif tags here.', 'PhotoQ');
+						?>
+						<ul class="exifTags">
+							<?php $this->_oc->render('exifOptions');?>
+						</ul>
+					</td>
+				</tr>
+			</table>
+			</div>
+			</div>
+			
+			
+			<div  class="postbox closed">
+			<h3 class="postbox-handle"><span><?php _e('Watermarking', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table noborder">
+			<tr valign="top">
+					<th scope="row"><?php _e('Watermark Image:', 'PhotoQ') ?></th>
+					<td>
+					<?php $this->showCurrentWatermark(); ?>
+			
+			<input style="vertical-align: top;" type="submit" class="button-secondary"
+					name="showWatermarkUploadPanel"
+					value="<?php _e('Change Watermark', 'PhotoQ') ?> &raquo;" />
+					</td>
+			</tr>
+			
+			<?php $this->_oc->render('watermarkOptions');?>
+			
+			</table>
+			</div>
+			</div>
+			
+			
+			
+			<div  class="postbox closed">
+			<h3 class="postbox-handle"><span><?php _e('Meta Fields', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+				<tr valign="top">
+					<th scope="row">
+						<?php _e('Upon Add:', 'PhotoQ'); ?>
+					</th>
+					<td>
+						<?php $this->_oc->render('fieldAddPosted');?>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<?php _e('Upon Delete:', 'PhotoQ'); ?>
+					</th>
+					<td>
+						<?php $this->_oc->render('fieldDeletePosted');?>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<?php _e('Upon Rename:', 'PhotoQ'); ?>
+					</th>
+					<td>
+						<?php $this->_oc->render('fieldRenamePosted');?>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th><?php _e('Defined Fields:', 'PhotoQ'); ?></th>
+					<td>
+						<table width="200" cellspacing="2" cellpadding="5"
+							class="meta_fields noborder">
+
+							<?php
+								$this->list_metafields('options');				
+							?>
+				
+						</table>
+					</td>
+				</tr>	
+			</table>
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table noborder">
+				<tr valign="top">
+					<th scope="row">
+						<label for="newFieldName">Name of new field: </label>
+					</th>
+					<td>
+						<input type="text" name="newFieldName" id="newFieldName"
+								size="20" maxlength="20" value="" />
+						<input type="submit" class="button-secondary"
+								name="addField"
+								value="<?php _e('Add Meta Field', 'PhotoQ') ?> &raquo;" />
+					</td>
+				</tr>
+			</table>
+			</div>
+			</div>
+		
+			<div  class="postbox closed">
+			<h3 class="postbox-handle"><span><?php _e('Further Options', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+				
+				<?php 
+				
+				$furtherOptions = array(
+					'imgdir' => __('Image Directory:', 'PhotoQ'),
+					'imagemagickPath' => __('ImageMagick Path:', 'PhotoQ'),
+					'cronJobs' => __('Automatic Posting:', 'PhotoQ'),
+					'qPostAuthor' => __('PhotoQ Post Author:', 'PhotoQ'),
+					'qPostDefaultCat' => __('PhotoQ Default Category:', 'PhotoQ'),
+					'foldCats' => __('Fold Categories:', 'PhotoQ'),
+					'showThumbs' => __('Admin Thumbs:', 'PhotoQ'),
+					'autoTitleRegex' => __('Auto Titles:', 'PhotoQ'),
+					'enableFtpUploads' => __('FTP Upload:', 'PhotoQ'),
+					'postMulti' => __('Second Post Button:', 'PhotoQ'),
+					'deleteImgs' => __('Deleting Posts:', 'PhotoQ'),
+					'enableBatchUploads' => __('Batch Uploads:', 'PhotoQ')
+				); 
+				
+				
+				foreach ($furtherOptions as $optName => $optLabel){
+					echo '<tr valign="top">';
+					echo '<th scope="row">'.$optLabel.'</th><td>';
+					$this->_oc->render($optName);
+					echo '</td></tr>';
+				}
+				
+				
+				?>
+				
+			</table>
+			</div>
+			</div>
+			
+			<div  class="postbox closed">
+			<h3 class="postbox-handle"><span><?php _e('Upgrading', 'PhotoQ') ?></span></h3>
+			<div class="inside">
+			<table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+				
+				<?php if(false): //eventually move this out completely?>
+				<tr valign="top">
+					<th scope="row"><?php _e('PhotoQ < 1.5.2:','PhotoQ') ?></th>
+					<td><label for="oldImgDir">Old Image Directory: </label>
+					<input type="text" name="oldImgDir" id="oldImgDir"
+					size="20" maxlength="20" value="" />
+					<input style="vertical-align: top;" type="submit" class="button-secondary"
+					name="showMoveImgDirPanel"
+					value="<?php _e('Move ImgDir to wp-content', 'PhotoQ') ?> &raquo;" /></td>
+				</tr>
+				<?php endif; ?>
+				
+				<tr valign="top">
+					<th scope="row"><?php _e('PhotoQ < 1.5:','PhotoQ') ?></th>
+					<td><input style="vertical-align: top;" type="submit" class="button-secondary"
+					name="showUpgradePanel"
+					value="<?php _e('Upgrade from PhotoQ < 1.5', 'PhotoQ') ?> &raquo;" /></td>
+				</tr>
+				
+			</table>
+			</div>
+			</div>
+		
+		
+		<?php 
+			if ( function_exists('wp_nonce_field') )
+					wp_nonce_field('photoq-updateOptions');
+		?>
+		
+		<p class="submit">
+			<input type="submit" name="info_update" class="button-primary"
+				value="<?php _e('Save Changes', 'PhotoQ') ?>" />
+		</p>
+		</div>
+	</form>
+</div> 
---
+++ wp-content/plugins/photoq-photoblog-plugin/panels/upload.php
@@ -0,0 +1,76 @@
+<?php ?>
+<div class=wrap>
+	<h2>Manage PhotoQ - Upload</h2>	
+	
+	<div id="commonInfo<?php if(isset($_POST['ftp_upload'])) echo 'Ftp'; ?>">		
+		<form id="batchedit" method="post" enctype="multipart/form-data" action="edit.php?page=whoismanu-photoq.php">
+		<div id="poststuff">
+		<h4>Enter common info:</h4>
+		<div class="photo_info">
+		
+		<div class="main info_group">
+			<div class="info_unit"><label>Description:</label><br /><textarea style="font-size:small;" name="img_descr" id="img_descr" cols="30" rows="3"></textarea></div>
+			
+			<?php //this makes it retro-compatible
+				if(function_exists('get_tags_to_edit')): ?>
+			<div class="info_unit"><label><?php _e('Tags (separate multiple tags with commas: cats, pet food, dogs):'); ?></label><br /><input type="text" name="tags_input" class="tags-input" id="tags-input" size="50"/></div>
+			<?php endif; ?>
+			
+		</div>
+		<?php
+			echo '<div class="info_group">';
+			$this->list_metafields('edit_queue');
+			echo '</div>';
+		?>
+		<ul class="wimpq_cats info_group"><?php $this->dropdown_categories(); ?></ul>
+		<br class="clr" />
+		
+		<?php
+			$submitLabel = isset($_POST['ftp_upload']) ? 'Import/Enter Info &raquo;' : 'Enter Info &raquo;';
+		?>
+		<p style="float: right" class="infobutton">
+			<input type="submit" class="button-primary action" name="edit_batch" value="<?php echo $submitLabel; ?>" />
+		</p>
+		</div>
+		<?php if(isset($_POST['ftp_upload'])) $this->showFtpFileList(); ?>
+		</div>
+		</form>
+		<div class="clr">&nbsp;</div>
+	</div>
+	
+<?php if(!isset($_POST['ftp_upload'])): ?>
+	<form action="edit.php?page=whoismanu-photoq.php" method="post" enctype="multipart/form-data">
+		
+		<div class="tablenav">
+		
+			<div class="alignleft actions">
+			<?php if($this->_oc->getValue('enableBatchUploads')): ?>
+				<div id="flash-browse-button"></div>
+			<?php else: ?>
+				<input type="file" class="button-secondary" name="Filedata" id="Filedata" />
+				<input type="submit" class="button-secondary action" value="Upload"/>
+				<input type="hidden" name="batch_upload" value="0">
+			<?php endif; ?>
+			</div>
+			<div class="alignleft actions">
+				<input type="button" id="cancelbtn" class="button-secondary action" onclick="cancelUpload()" value="Cancel" />		
+			</div>
+			<div class="alignright actions">
+			<?php if($this->_oc->getValue('enableFtpUploads')): ?>
+				<input type="submit" name="ftp_upload" class="button-secondary action" value="Import from ftp directory..." />
+			<?php endif; ?>
+			</div>
+		</div>
+	</form>
+	
+	<div id="SWFUploadFileListingFiles"></div>
+	
+	<br class="clr" />
+	
+	
+	<br /><br />
+
+<?php endif;?>	
+		
+</div>
+
